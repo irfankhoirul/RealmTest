4e092e6554930d60098c6a4658ac22b71670531f
Irfan Khoirul Muhlishin <irfankhoirul@gmail.com>
2016-05-01 10:42:38 +0700

    Update

--- app/src/main/java/com/muhlish/apps/realmtest/MainActivity.java
+++ app/src/main/java/com/muhlish/apps/realmtest/MainActivity.java
@@ -3,7 +3,6 @@ package com.muhlish.apps.realmtest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button btInputData;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         RealmQuery<Mahasiswa> query = realm.where(Mahasiswa.class);
         // Execute the query:
-        RealmResults<Mahasiswa> result1 = query.findAll();
+        RealmResults<Mahasiswa> result = query.findAll();
 
-        if (result1.size() == 0) {
+        if (result.size() == 0) {
             btInputData.setVisibility(View.VISIBLE);
             btInputData.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -59,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             llDataMahasiswa.setVisibility(View.VISIBLE);
 
-            nim = result1.get(0).getNim();
-            name = result1.get(0).getNama();
-            department = result1.get(0).getJurusan();
-            year = result1.get(0).getAngkatan();
+            nim = result.get(0).getNim();
+            name = result.get(0).getNama();
+            department = result.get(0).getJurusan();
+            year = result.get(0).getAngkatan();
 
             Mahasiswa data = new Mahasiswa();
             data.setNim(nim);
@@ -75,9 +73,8 @@ public class MainActivity extends AppCompatActivity {
             tvDepartment.setText(data.getJurusan());
             tvYear.setText(String.valueOf(data.getAngkatan()));
 
-            for (int i = 0; i < result1.size(); i++) {
-                Log.v("Data" + i, result1.get(i).toString());
-            }
+            updateNama(realm, data, "Ahmad");
+
         }
 
     }
@@ -86,5 +83,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         this.recreate();
+    }
+
+    public void deleteDataByNim(Realm realm, RealmResults<Mahasiswa> result, String nim) {
+        int location = 0;
+        for (int i = 0; i < result.size(); i++) {
+            if (result.get(i).getNim().equalsIgnoreCase(nim)) {
+                location = i;
+            }
+        }
+
+        realm.beginTransaction();
+        result.deleteFromRealm(location);
+        realm.commitTransaction();
+    }
+
+    public void updateNama(Realm realm, Mahasiswa data, String nama) {
+        Mahasiswa toEdit = realm.where(Mahasiswa.class).equalTo("nim", data.getNim()).findFirst();
+        realm.beginTransaction();
+        toEdit.setNama(nama);
+        toEdit.setNim(data.getNim());
+        toEdit.setJurusan(data.getJurusan());
+        toEdit.setAngkatan(data.getAngkatan());
+        realm.commitTransaction();
     }
 }
